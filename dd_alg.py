01fbc9d2bff251ed493d274c587e07e2b0219c2b
lucasalavapena <lucas.alavapena@gmail.com>
2020-07-24 03:14:45 +0100

    Fixed anchor day function calculation

--- dd_alg.py
+++ dd_alg.py
@@ -5,18 +5,39 @@ import datetime
 import math
 
 def rand_date(earliest_date, latest_date):
-    differenceDateSeconds = (latest_date - earliest_date).total_seconds()
-    date = earliest_date + datetime.timedelta(seconds=random.randint(0, int(differenceDateSeconds)))
+    """
+    generates a random date between the two parameters given
+    :param earliest_date:
+    :param latest_date:
+    :return: a random date between the two parameters given
+    """
+    difference_date_seconds = (latest_date - earliest_date).total_seconds()
+    date = earliest_date + datetime.timedelta(seconds=random.randint(0, int(difference_date_seconds)))
     return date
 
 def correct_weekday(date):
+    """
+
+    :param date: date using datetime
+    :return: weekday in 0-6 format
+    """
     return (date.weekday() + 1) % 7
 
 def anchor_day(year):
+    """
+
+    :param year:
+    :return:
+    """
     century = math.floor(year/100)
-    return 5 * (century % 2) % 7 + 2
+    return (5 * (century % 4) % 7 + 2) % 7
 
 def is_leap_year(year):
+    """
+
+    :param year:
+    :return:
+    """
     if year % 4 == 0:
         if year % 100 == 0:
             return year % 400 == 0
@@ -24,12 +45,17 @@ def is_leap_year(year):
 
 
 def worked_date(date):
+    """
+
+    :param date:
+    :return:
+    """
     # Initialised variableshttps://en.wikipedia.org/wiki/Doomsday_rule#The_%22odd_+_11%22_method
     # hardcoded days can use: anchor_day(year)
     anchor_days = {1800: 5,
-                  1900: 3,
-                  2000: 2,
-                  2100: 0}
+                   1900: 3,
+                   2000: 2,
+                   2100: 0}
 
     dayOfWeek = {0: "Sunday",
                  1: "Monday",
@@ -40,18 +66,18 @@ def worked_date(date):
                  6: "Saturday"}
 
     # need ddalg_step2 better way of doign this
-    DoomsdayNonLeap = {1: [3, 10, 17, 24, 31],
-                       2: [7, 14, 21, 28],
-                       3: [7, 14, 21, 28],
-                       4:  [4, 11, 18, 21, 28],
-                       5:  [2, 9, 16, 23, 30],
-                       6:  [6, 13, 20, 27],
-                       7:  [4, 11, 18, 25],
-                       8:  [1, 8, 15, 22, 29],
-                       9:  [5, 12, 19, 26],
-                       10: [3, 10, 17, 24, 31],
-                       11: [7, 14, 21, 28],
-                       12: [5, 12, 19, 26]}
+    doomsday_non_leap = {1: [3, 10, 17, 24, 31],
+                         2: [7, 14, 21, 28],
+                         3: [7, 14, 21, 28],
+                         4:  [4, 11, 18, 21, 28],
+                         5:  [2, 9, 16, 23, 30],
+                         6:  [6, 13, 20, 27],
+                         7:  [4, 11, 18, 25],
+                         8:  [1, 8, 15, 22, 29],
+                         9:  [5, 12, 19, 26],
+                         10: [3, 10, 17, 24, 31],
+                         11: [7, 14, 21, 28],
+                         12: [5, 12, 19, 26]}
     #
     # DoomsdayLeap = {1: [4, 11, 18, 25],
     #                 2: [1, 8, 15, 22, 29],
@@ -77,13 +103,14 @@ def worked_date(date):
 
     anchorday_key = math.floor(date.year / 100) * 100
     anchor_day = anchor_days.get(anchorday_key)
-    help_text += """2. The anchor day is {0} as the date is in the {1}s. This can either be memorized
-     or calculated using: 5 * (century % 2) % 7 + 2.\n \t In this case: 5 * ({2} % 2) % 7 + 2 = {0}
-     \n""".format(anchor_day, anchorday_key, int(anchorday_key/100))
+    help_text += """2. The anchor day is {0} as the date is in the {1}s.
+    This can either be memorized or calculated using: 5 * (century % 2) % 7 + 2.
+     \n \t In this case: 5 * ({2} % 2) % 7 + 2 = {0}\n""".format(
+         anchor_day, anchorday_key, int(anchorday_key/100))
 
     doomsday = ((ddalg_step2+ddalg_step3+ddalg_step4) % 7 + anchor_day) % 7
 
-    help_text +=  """3. Time to compute the doomsday :)).
+    help_text += """3. Time to compute the doomsday :)).
      First lets compute some steps (ddalg_step2,ddalg_step3,ddalg_step4)
      of the bigger computation as to not lose our mind. \n
     ddalg_step2. The last two digits of the year in question is {0},
@@ -93,21 +120,23 @@ def worked_date(date):
     This lets us compute the doomsday:\n
     \t ((ddalg_step2+ddalg_step3+ddalg_step4) % 7 + anchor_day) \n
     Therefore in this case: \n
-    \t doomsday = (({1}+{2}+{3}) % 7 + {4}) % 7 = {5} \n""".format(ddalg_step1, ddalg_step2, ddalg_step3, ddalg_step4, anchor_day, doomsday)
+    \t doomsday = (({1}+{2}+{3}) % 7 + {4}) % 7 = {5} \n""".format(
+        ddalg_step1, ddalg_step2, ddalg_step3, ddalg_step4, anchor_day, doomsday)
     # format(ddalg_step1, ddalg_step2, ddalg_step3, ddalg_step4, anchor_day, doomsday)
 
     # print('Year: {} vs doomsday: {}'.format(currentDate.year,doomsday))
 
     help_text += """4. If we are in the first two months of the year and are in ddalg_step2
-    leap year we need to use ddalg_step2 different set of doomsday for that year so lets check that first."""
+    leap year we need to use ddalg_step2 different set of doomsday for that year 
+    so lets check that first."""
     if is_leap_year(date.year) and date.month < 3:
-        closet_doomsday = DoomsdayNonLeap.get(date.month)[0]
+        closet_doomsday = doomsday_non_leap.get(date.month)[0]
         day_of_week = ((date.day - closet_doomsday) + doomsday) % 7
-        help_text +="The year {} is ddalg_step2 leap year.\n".format(date.year)
+        help_text += "The year {} is ddalg_step2 leap year.\n".format(date.year)
     else:
-        closet_doomsday = DoomsdayNonLeap.get(date.month)[0]
+        closet_doomsday = doomsday_non_leap.get(date.month)[0]
         day_of_week = ((date.day - closet_doomsday) + doomsday) % 7
-        help_text +="The year {} is not ddalg_step2 leap year.\n".format(date.year)
+        help_text += "The year {} is not ddalg_step2 leap year.\n".format(date.year)
 
     help_text += "5. Now we must find the closest doomsday to the {}, that in this case would be {}.\n".format(
         date_str, closet_doomsday)
@@ -120,7 +149,17 @@ def worked_date(date):
     return help_text
 
 def main():
-     worked_date(datetime.datetime(1922, 3, 1, 0, 0, 0))
+    """
+
+    :return:
+    """
+    # worked_date(datetime.datetime(1922, 3, 1, 0, 0, 0))
+    print(anchor_day(1800))
+    print(anchor_day(1932))
+    print(anchor_day(2012))
+    print(anchor_day(1723))
+
+    # this is wrong
 
 if __name__ == '__main__':
     main()
